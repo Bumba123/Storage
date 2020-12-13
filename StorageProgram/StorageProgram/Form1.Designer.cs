fde183d1d25dbb0c0bddfd34edf42b2c672a2bac
Bumba123 <russian.empire.99@mail.ru>
2020-12-13 15:50:41 +0300

    1

---
+++ StorageProgram/StorageProgram/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace StorageProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.login = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.storages = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(27, 33);
+            this.dgv.MultiSelect = false;
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.Size = new System.Drawing.Size(469, 332);
+            this.dgv.TabIndex = 0;
+            this.dgv.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dgv_RowsAdded);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(520, 426);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.login);
+            this.tabPage1.Controls.Add(this.password);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(512, 400);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Авторизация";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(192, 107);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(100, 20);
+            this.login.TabIndex = 4;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(192, 175);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(100, 20);
+            this.password.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Пароль";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Логин";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(217, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Войти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.storages);
+            this.tabPage2.Controls.Add(this.dgv);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(512, 400);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Склады";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(149, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Добавить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(421, 371);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(241, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Склад";
+            // 
+            // storages
+            // 
+            this.storages.FormattingEnabled = true;
+            this.storages.Location = new System.Drawing.Point(296, 6);
+            this.storages.Name = "storages";
+            this.storages.Size = new System.Drawing.Size(200, 21);
+            this.storages.TabIndex = 1;
+            this.storages.SelectedIndexChanged += new System.EventHandler(this.storages_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 441);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Учет складов";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox login;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox storages;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ StorageProgram/StorageProgram/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StorageProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Подключение к БД
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con = new SQLiteConnection("Data Source=" + Application.StartupPath + "\\Storages.db");
+            con.Open();
+            sda = new SQLiteDataAdapter("SELECT * FROM [Пользователи] Where [Логин]='" + login.Text + "' AND [Пароль]='" + password.Text + "';", con);
+            dt = new DataTable();
+
+            if (sda.Fill(dt) == 0)
+            {
+                button2.Visible = false;
+                dgv.ReadOnly = true;
+                MessageBox.Show("Неверный логин или пароль!", "Произошла ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (dt.Rows[0][1].ToString()=="admin")
+                {
+                    button2.Visible = true;
+                    dgv.ReadOnly = false;
+                    button3.Visible = true;
+                }
+                else
+                {
+                    button2.Visible = false;
+                    dgv.ReadOnly = true;
+                    button3.Visible = false;
+                }
+
+                UpadteCB();
+                tabControl1.SelectedIndex = 1;
+            }
+        }
+
+        /// <summary>
+        /// Вывод складов в список
+        /// </summary>
+        void UpadteCB()
+        {
+            sda = new SQLiteDataAdapter("SELECT * FROM Склады", con);
+            dt = new DataTable();
+            sda.Fill(dt);
+
+            storages.Items.Clear();
+            countStorahes = 0;
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                storages.Items.Add(dt.Rows[i][1].ToString());
+                countStorahes++;
+            }
+            storages.SelectedIndex = storages.Items.Count - 1;
+            storages_SelectedIndexChanged(storages,null);
+        }
+
+        static int countStorahes = 0;
+
+        SQLiteDataAdapter mainsda;
+        DataTable maindt;
+
+        SQLiteDataAdapter sda;
+        SQLiteConnection con;
+        DataTable dt;
+        SQLiteCommand com;
+
+        //Событие закрытия формы
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (con != null)
+                con.Close();
+        }
+
+        /// <summary>
+        /// Номер склада
+        /// </summary>
+        static int numberStorage = 0;
+
+        /// <summary>
+        /// Обновление таблицы при изменении склада
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void storages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sda = new SQLiteDataAdapter("SELECT * FROM Склады WHERE [Склады].[Название]=\""+storages.Text+"\"", con);
+            dt = new DataTable();
+            sda.Fill(dt);
+
+            numberStorage = int.Parse(dt.Rows[0][0].ToString());
+
+            mainsda = new SQLiteDataAdapter("SELECT * FROM  [Товары] " +
+              "WHERE  [Товары].[Код склада]=" + numberStorage, con);
+
+
+            maindt = new DataTable();
+
+            mainsda.Fill(maindt);
+            dgv.DataSource = maindt;
+            dgv.Columns[0].Visible = false;
+            dgv.Columns[1].Visible = false;
+            dgv.ClearSelection();
+        }
+
+        /// <summary>
+        /// Создание нового склада
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim(' ') != "" && con!=null)
+            {
+                countStorahes++;
+                com = new SQLiteCommand("INSERT INTO [Склады] VALUES("+countStorahes+",\""+ textBox1.Text.Trim(' ') + "\");", con);
+                com.ExecuteNonQuery();
+                UpadteCB();
+            }
+        }
+
+        /// <summary>
+        /// Обновление таблицы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SQLiteCommandBuilder scb = new SQLiteCommandBuilder(mainsda);
+            mainsda.Update(maindt);
+            index = 0;
+        }
+
+        /// <summary>
+        /// Максимальный элемент
+        /// </summary>
+        static int index = 0;
+
+        /// <summary>
+        /// Индексирует строки
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dgv_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            DataGridView buffer = (DataGridView)sender;
+
+            if (buffer.SelectedCells.Count != 0)
+            {
+                if (buffer.SelectedCells[0].RowIndex == dgv.RowCount-2)
+                {
+                    sda = new SQLiteDataAdapter("SELECT Max(Код) FROM Товары", con);
+                    dt = new DataTable();
+                    sda.Fill(dt);
+
+                    index++;
+                    dgv.Rows[buffer.SelectedCells[0].RowIndex].Cells[1].Value = numberStorage.ToString();
+                    dgv.Rows[buffer.SelectedCells[0].RowIndex].Cells[0].Value = int.Parse((dt.Rows[0][0]).ToString()) + index;
+                }
+            }
+
+        }
+    }
+}
